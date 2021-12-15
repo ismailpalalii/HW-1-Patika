2c711e08ccfc0df84c0e182239f5b8044fb41103
Ismail Palali <ismail.palali.pp@gmail.com>
2021-12-15 16:10:31 +0300

    Completed Algorithm

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+func fib( n: Int) -> Int {
+    // Write a function for the Fibonacci numbers.
+
+    if n < 2 {
+        return n
+    }else {
+        return fib(n: n-1) + fib(n: n-2)
+    }
+}
+
+for i in 0..<1000 {
+    //Fibonacci numbers between 750 and 100 are listed and printed.
+    
+     let result = fib(n: i)
+    if result > 750 && result < 1000{
+        print("Fibonacci Number = \(result)")
+        break
+    }
+}
+
